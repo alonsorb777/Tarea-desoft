6442810d25e5640f44a4bfe3b7e682be1436c0c0
Alonso Robredo <Alonso.robredo@usach.cl>
2026-08-23 04:45:38 -0400

    Optimización completa

--- src/segmentacion/sam_segmentacion.py
+++ src/segmentacion/sam_segmentacion.py
@@ -395,9 +395,9 @@ def generar_mascaras(
     predictor,
     imagen,
     points_per_side=32,
-    pred_iou_thresh=0.75
-    stability_score_thresh=0.85
-    min_mask_region_area=75
+    pred_iou_thresh=0.75,
+    stability_score_thresh=0.85,
+    min_mask_region_area=75,
     crop_n_layers=0.25
 ):
     """
