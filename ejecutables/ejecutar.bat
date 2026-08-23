e0b5b804fd564a6211fe44b8f2b3fb7ecb7644d4
Alonso Robredo <Alonso.robredo@usach.cl>
2026-08-23 04:37:22 -0400

    Hacemos que el usiario descargue el zip y pueda instalar todo lo necesario solo con 2 clicks

--- ejecutables/ejecutar.bat
+++ ejecutables/ejecutar.bat
@@ -1,10 +1,23 @@
 @echo off
 echo ===================================================
-echo   Instalando librerias e iniciando la aplicacion
+echo   Iniciando Segmentacion de Discos Protoplanetarios
 echo ===================================================
 
-cd ..
+::1. Crear entorno virtual si no existe
+if not exist "venv" (
+    echo Creando entorno virtual de Python...
+    python -m venv venv
+)
+
+:: 2. Activar entorno virtual
+call venv\Scripts\activate.bat
+
+:: 3. Instalar/Actualizar dependencias
+echo Verificando e instalando librerias necesarias...
 pip install -r requirements.txt
-python -m streamlit run demo/demo_sam.py
+
+:: 4. Lanzar la app de Streamlit
+echo Lanzando aplicacion en el navegador...
+streamlit run demo/demo_sam.py
 
 pause
--- ejecutables/ejecutar.sh
+++ ejecutables/ejecutar.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 echo "==================================================="
-echo "  Instalando librerias e iniciando la aplicacion..."
+echo "  Iniciando Segmentacion de Discos Protoplanetarios"
 echo "==================================================="
 
-cd ..
+# 1. Crear entorno virtual si no existe
+if [ ! -d "venv" ]; then
+    echo "Creando entorno virtual de Python..."
+    python3 -m venv venv
+fi
+
+# 2. Activar entorno virtual
+source venv/bin/activate
+
+# 3. Instalar dependencias
+echo "Verificando e instalando librerias necesarias..."
 pip install -r requirements.txt
-python3 -m streamlit run demo/demo_sam.py
+
+# 4. Lanzar la app
+echo "Lanzando aplicacion en el navegador..."
+streamlit run demo/demo_sam.py
